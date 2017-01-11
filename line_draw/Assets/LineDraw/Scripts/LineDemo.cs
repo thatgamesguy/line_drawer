c26bd5746f3dcf78e945b1f5e406505e8c0d5591
Robert Wells <robert.wells@gandhigames.co.uk>
2017-01-11 21:54:57 +0000

    initial commit - working

---
+++ line_draw/Assets/LineDraw/Scripts/LineDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDemo : MonoBehaviour 
+{
+	public LineFactory lineDraw;
+
+	private Vector2 start;
+	private Line drawnLine;
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			var pos = Camera.main.ScreenToWorldPoint (Input.mousePosition); // Start line drawing
+			drawnLine = lineDraw.GetLine (pos, pos, 0.02f, Color.white);
+		} else if (Input.GetMouseButtonUp (0)) {
+			drawnLine = null; // End line drawing
+		}
+
+		if (drawnLine != null) {
+			drawnLine.end = Camera.main.ScreenToWorldPoint (Input.mousePosition); // Update line end
+		}
+	}
+}
---
+++ line_draw/Assets/LineDraw/Scripts/LineFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineFactory : MonoBehaviour
+{
+	public GameObject linePrefab;
+
+	public int numToPool = 50;
+
+	private Line[] pooledLines;
+	private int currentIndex = 0;
+
+	void Start ()
+	{
+		pooledLines = new Line[numToPool];
+		
+		for (int i = 0; i < numToPool; i++) {
+			var line = Instantiate (linePrefab);
+			line.SetActive (false);
+			line.transform.SetParent (transform);
+			pooledLines[i] = line.GetComponent<Line> ();
+		}
+	}
+
+	public Line GetLine (Vector2 start, Vector2 end, float width, Color color)
+	{
+		var line = pooledLines [currentIndex];
+		
+		line.Initialise (start, end, width, color);
+		line.gameObject.SetActive (true);
+
+		currentIndex = (currentIndex + 1) % pooledLines.Length;
+
+		return line;
+
+	}
+
+}
