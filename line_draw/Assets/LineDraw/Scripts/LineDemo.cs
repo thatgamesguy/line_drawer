879928962f0bdd8d5dcf40f9615c487958b40e26
Robert Wells <robert.wells@gandhigames.co.uk>
2017-01-12 13:13:01 +0000

    documentation and comments

--- line_draw/Assets/LineDraw/Scripts/LineDemo.cs
+++ line_draw/Assets/LineDraw/Scripts/LineDemo.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class LineDemo : MonoBehaviour 
 {
-	public LineFactory lineDraw;
+	public LineFactory lineFactory;
 
 	private Vector2 start;
 	private Line drawnLine;
 
-	void Update () 
+	void Update ()
 	{
 		if (Input.GetMouseButtonDown (0)) {
 			var pos = Camera.main.ScreenToWorldPoint (Input.mousePosition); // Start line drawing
-			drawnLine = lineDraw.GetLine (pos, pos, 0.02f, Color.white);
+			drawnLine = lineFactory.GetLine (pos, pos, 0.02f, Color.white);
 		} else if (Input.GetMouseButtonUp (0)) {
 			drawnLine = null; // End line drawing
 		}
 
 		if (drawnLine != null) {
 			drawnLine.end = Camera.main.ScreenToWorldPoint (Input.mousePosition); // Update line end
+		}
+	}
+
+	/// <summary>
+	/// Get a list of active lines and deactivates them.
+	/// </summary>
+	public void Clear()
+	{
+		var activeLines = lineFactory.GetActive ();
+
+		foreach (var line in activeLines) {
+			line.gameObject.SetActive(false);
 		}
 	}
 }
--- line_draw/Assets/LineDraw/Scripts/LineFactory.cs
+++ line_draw/Assets/LineDraw/Scripts/LineFactory.cs
@@ -2,20 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Stores a pool of Line. Allows retrieval and initialisation of pooled line.
+/// </summary>
 public class LineFactory : MonoBehaviour
 {
+	/// <summary>
+	/// The line prefab.
+	/// </summary>
 	public GameObject linePrefab;
 
-	public int numToPool = 50;
+	/// <summary>
+	/// The number to pool. This is the maximum number of lines that can be retrieve from this factory.
+	/// When a number of lines greater that this number is requested previous lines are overwritten.
+	/// </summary>
+	public int maxLines = 50;
 
 	private Line[] pooledLines;
 	private int currentIndex = 0;
 
 	void Start ()
 	{
-		pooledLines = new Line[numToPool];
+		pooledLines = new Line[maxLines];
 		
-		for (int i = 0; i < numToPool; i++) {
+		for (int i = 0; i < maxLines; i++) {
 			var line = Instantiate (linePrefab);
 			line.SetActive (false);
 			line.transform.SetParent (transform);
@@ -23,6 +33,14 @@ public class LineFactory : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// Gets an initialised and active line. The line is retrieved from the pool and set active.
+	/// </summary>
+	/// <returns>The active line.</returns>
+	/// <param name="start">Start position in world space.</param>
+	/// <param name="end">End position in world space.</param>
+	/// <param name="width">Width of line.</param>
+	/// <param name="color">Color of line.</param>
 	public Line GetLine (Vector2 start, Vector2 end, float width, Color color)
 	{
 		var line = pooledLines [currentIndex];
@@ -33,7 +51,23 @@ public class LineFactory : MonoBehaviour
 		currentIndex = (currentIndex + 1) % pooledLines.Length;
 
 		return line;
+	}
 
+	/// <summary>
+	/// Returns all active lines.
+	/// </summary>
+	/// <returns>The active lines.</returns>
+	public List<Line> GetActive()
+	{
+		var activeLines = new List<Line> ();
+
+		foreach (var line in pooledLines) {
+			if (line.gameObject.activeSelf) {
+				activeLines.Add (line);
+			}
+		}
+
+		return activeLines;
 	}
 
 }
